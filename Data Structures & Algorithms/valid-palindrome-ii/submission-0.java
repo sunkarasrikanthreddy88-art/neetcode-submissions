fee0224ec994369d11426f2c818ed2c153e44f90
Sunkara Srikanth Reddy <sunkarasrikanthreddy88@gmail.com>
2026-07-23 06:15:39 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<=right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return check(s,left+1,right)||check(s,left,right-1);
+            }
+        }
+        return true;
+    }
+    public boolean check(String s,int left,int right){
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
